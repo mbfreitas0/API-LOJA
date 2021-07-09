273014220b1e83a606ba0901095fcce9362b1dd6
mbfreitas0 <mbfreitas0@gmail.com>
2021-07-09 19:24:28 -0300

    09072021-1924

--- controllers/produtos-controller.js
+++ controllers/produtos-controller.js
@@ -1,6 +1,39 @@
 const mysql = require('../mysql').pool;
 
-exports.getProdutos = (req, res, next) => {
+exports.getProduto = async (req, res, next)=> {
+    try {
+        const query = 'SELECT * FROM produtos;';
+        const result = await mysql.execute(query, [req.params.id]);
+
+        if (result.length == 0) {
+            return res.status(404).send({
+                message: 'Não foi encontrado produto com este ID'
+            })
+        }
+        const response = {
+            product: {
+                id: result[0].id,
+                id_grupo:result[0].id_grupo,
+                id_marca:result[0].id_marca,
+                id_locacao:result[0].id_locacao,
+                status:result[0].status,
+                descricao: result[0].descricao,
+                estoque_min: result[0].estoque_min,
+                estoque_max: result[0].estoque_max, 
+                request: {
+                    type: 'GET',
+                    description: 'Retorna todos os produtos',
+                    url: process.env.URL_API + 'produtos'
+                }
+            }
+        }
+        return res.status(200).send(response);
+    } catch (error) {
+        return res.status(500).send({ error: error });
+    }
+};
+
+/* exports.getProdutos = (req, res, next) => {
     mysql.getConnection((error, conn) =>{
         if(error){return res.status(500).send({ error:error })}
         conn.query(
@@ -27,9 +60,43 @@ exports.getProdutos = (req, res, next) => {
             }
         )
     });
-}
+} */
 
-exports.getUmproduto = (req, res, next) => {
+exports.getUmproduto = async (req, res, next)=> {
+    try {
+        const query = 'SELECT * FROM produtos WHERE id = ?;';
+        const result = await mysql.execute(query, [req.params.id]);
+
+        if (result.length == 0) {
+            return res.status(404).send({
+                message: 'Não foi encontrado produto com este ID'
+            })
+        }
+        const response = {
+            product: {
+                id: result[0].id,
+                id_grupo:result[0].id_grupo,
+                id_marca:result[0].id_marca,
+                id_locacao:result[0].id_locacao,
+                status:result[0].status,
+                descricao: result[0].descricao,
+                estoque_min: result[0].estoque_min,
+                estoque_max: result[0].estoque_max, 
+                request: {
+                    type: 'GET',
+                    description: 'Retorna todos os produtos',
+                    url: process.env.URL_API + 'produtos'
+                }
+            }
+        }
+        return res.status(200).send(response);
+    } catch (error) {
+        return res.status(500).send({ error: error });
+    }
+};
+
+
+/* exports.getUmproduto = (req, res, next) => {
     mysql.getConnection((error, conn) =>{
         if(error){return res.status(500).send({ error:error })}
         conn.query(
@@ -68,9 +135,46 @@ exports.getUmproduto = (req, res, next) => {
             }
         )
     });
-}
+} */
 
-exports.postProduto = (req, res, next) =>{
+exports.postProduto = async (req, res, next) => {
+    try {
+        const query = 'INSERT INTO produtos(id_grupo, id_marca, id_locacao, status, descricao, estoque_min, estoque_max) VALUES (?, ?, ?, ?, ?, ?, ?)',
+        const result = await mysql.execute(query, [
+            req.body.id_grupo,
+            req.body.id_marca,
+            req.body.id_locacao,
+            req.body.status,
+            req.body.descricao,
+            req.body.estoque_min,
+            req.body.estoque_max,
+        ]);
+
+        const response = {
+            message: 'Produto inserido com sucesso',
+            createdProduct: {
+                        id: result.id,
+                        id_grupo: req.body.id_grupo,
+                        id_marca: req.body.id_marca,
+                        id_locacao: req.body.id_locacao,
+                        status: req.body.status,
+                        descricao: req.body.descricao,
+                        estoque_min: req.body.estoque_min,
+                        estoque_max: req.body.estoque_max,
+                request: {
+                    type: 'GET',
+                    description: 'Retorna todos os produtos',
+                    url: process.env.URL_API + 'produtos'
+                }
+            }
+        }
+        return res.status(201).send(response);
+    } catch (error) {
+        return res.status(500).send({ error: error });
+    }
+};
+
+/* exports.postProduto = (req, res, next) =>{
     console.log(req.file);
     mysql.getConnection((error, conn) =>{
         if(error){return res.status(500).send({ error : error })}
@@ -99,7 +203,7 @@ exports.postProduto = (req, res, next) =>{
             }
         )   
     });
-}
+} */
 
 // Modificar produto
 exports.updateProduto = async (req, res, next) => {
@@ -195,7 +299,38 @@ exports.updateProduto = async (req, res, next) => {
     });
 } */
 
-exports.deleteProduto = (req, res, next) =>{
+exports.deleteProduto = async (req, res, next) => {
+    try {
+        const query = `DELETE FROM produtos WHERE id = ?`;
+        await mysql.execute(query, [req.params.id]);
+
+        const response = {
+            message: 'Produto removido com sucesso',
+            request: {
+                type: 'POST',
+                description: 'Insere um produto',
+                url: process.env.URL_API + 'produtos',
+                body: {
+                    id_grupo: req.body.id_grupo,
+                    id_marca: req.body.id_marca,
+                    id_locacao: req.body.id_locacao,
+                    status: req.body.status,
+                    descricao: req.body.descricao,                        
+                    estoque_min: req.body.estoque_min,
+                    estoque_max: req.body.estoque_max
+                }
+            }
+        }
+        return res.status(202).send(response);
+
+    } catch (error) {
+        return res.status(500).send({ error: error });
+    }
+};
+
+
+
+/* exports.deleteProduto = (req, res, next) =>{
     mysql.getConnection((error, conn) =>{
         if(error){return res.status(500).send({ error : error })}
         conn.query(
@@ -227,4 +362,4 @@ exports.deleteProduto = (req, res, next) =>{
             }
         )   
     });
-}
+} */
